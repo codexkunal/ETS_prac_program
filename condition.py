3f85a248dd17466a66381b4e7ba0c32f9586eae4
Kunal Singh <70837417+codexkunal@users.noreply.github.com>
2023-07-28 08:58:46 +0530

    Update condition.py

--- condition.py
+++ condition.py
@@ -1,13 +1,3 @@
-first_char = input("enter the number :")
-
-if first_char == 'a':
-    print("you have entered ", first_char)
-elif first_char == 'b':
-    print("you have entered ", first_char)
-elif first_char == 'c':
-    print("you have entered ", first_char)
-else:
-    print("you have entered invalid character")
 
 
 
