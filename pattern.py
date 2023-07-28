6ef8f70a095e32ee2970cfbfb33ff905412a54de
codexkunal <sengarkunal833@gmail.com>
2023-07-28 08:57:10 +0530

    added

--- pattern.py
+++ pattern.py
@@ -45,3 +45,11 @@ for i in range(0,num+1):
         else:
             print(end=" ")
     print()
+"""counting numbers"""
+
+n = 1
+for i in range(1, num+1):
+    for j in range(1, i+1):
+        print(n, end='')
+        n = n+1
+    print()
