deb955a3d824a4a0f05601fde80eadbb31dac99b
codexkunal <sengarkunal833@gmail.com>
2023-07-30 23:57:48 +0530

    f

---
+++ while_loop.py
@@ -0,0 +1,35 @@
+"""try:
+  while i < 5:
+    print("hello")
+    i+= 1
+  else:
+    print('done')
+except:
+  print('please enter right value')"""
+
+
+"""1"""
+
+"""try:
+  while True:
+    n = int(input("please enter number :"))
+    print(n)
+    break
+except:
+  print("please enter the correct value:")"""
+
+"""2) example of while loop"""
+ 
+i = 0
+while(i<11):
+    print(f'2 * {i} = {2*i}')
+    i += 1
+
+"""3)"""
+i = int(input('please enter the number :'))
+while(i > -1):
+    print(i)
+    i -= 1
+else:
+    print("done")
+
